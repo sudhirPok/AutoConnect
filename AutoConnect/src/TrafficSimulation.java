32d20be04b5bb1c06165edc2aa6afdb0c4785b22
Sudhir Pokhrel <sud7scholar@gmail.com>
2019-11-04 00:07:27 -0500

    Adding functionality to parse raw traffic input into indindividual CSV files, and have each vehicle parse its own traffic data

--- AutoConnect/src/TrafficSimulation.java
+++ AutoConnect/src/TrafficSimulation.java
@@ -1,33 +1,66 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class TrafficSimulation {
-    int numVehicles = 0;
+
+    ArrayList<Vehicle> vehicleArrayList = new ArrayList();
 
 
+    /**
+     *
+     */
+    //This method takes in a CSV file containing the raw traffic simulation data for n vehicles,
+    //and creates n smaller CSV files per vehicle. Returns file path of created CSV files.
+    private ArrayList<String> readTrafficData(String pathToCsv) throws IOException{
+        ArrayList<String> vehicleData = new ArrayList<>();
 
+        String row;
 
-
-
-
-    private void readTrafficData(){
-        String row = "";
         BufferedReader csvReader = new BufferedReader((new FileReader(pathToCsv)));
+        csvReader.skip(1);
+
+        FileWriter writer = null;
+
+        int counter = 0;
         while((row = csvReader.readLine()) != null){
             String[] data = row.split(",");
 
-            //error handling
-            if (data.length == 0) continue;
-
-            //skip first trajector
-            if (data[0].contains("ID")) continue;
-
-            //handle new car
+            //if we encounter a car id, stop writing to current file & write to new file.
+            //note, this skips the current line
             if (Character.isDigit(data[0].charAt(0))){
-                count++;
-                continue;
+                if(writer!=null){
+                    writer.flush();
+                    writer.close();
+                }
+
+                String filePath = "vehicleData" + Integer.toString(++counter);
+                Path vehicleFilePath = Paths.get(filePath);
+                Files.createFile(vehicleFilePath);
+                vehicleData.add(filePath);
+                writer = new FileWriter(filePath);
+            }else{
+                writer.write(row);
             }
         }
+
+        writer.flush();
+        writer.close();
+
+        return vehicleData;
     }
+
+    private void createVehicles(ArrayList<String> vehicleCSVs){
+        for(String csvFile: vehicleCSVs){
+            Vehicle vehicle = new Vehicle(csvFile);
+            vehicleArrayList.add(vehicle);
+        }
+    }
+
+
 }
--- AutoConnect/src/Vehicle.java
+++ AutoConnect/src/Vehicle.java
@@ -1,13 +1,61 @@
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.UUID;
+
 public class Vehicle {
+    private HashMap<Float, Coordinate> timestamp = null;
+    private final UUID ID;
 
-    ArrayList<Float[]> pointData = new ArrayList();
+    private class Coordinate{
+        private final double latitude;
+        private final double longitude;
 
-    public Vehicle(File file){
+        private Coordinate(double x, double y){
+            latitude = x;
+            longitude = y;
+        }
+
+        public double getLatitute(){
+            return latitude;
+        }
+
+        public double getLongitute(){
+            return longitude;
+        }
+    }
+
+
+    public Vehicle(String file){
+        ID = UUID.randomUUID();
+        timestamp = new HashMap<>();
+    }
+
+    private void parsePointData(String file) throws IOException, ParseVehicleDataException{
+        BufferedReader csvReader = new BufferedReader(new FileReader(file));
+        String row = "";
+        while ((row = csvReader.readLine()) != null) {
+            String[] data = row.split(",", 4);
+            if(data.length!=4){
+                throw new ParseVehicleDataException("Raw vehicle data should contain empty space" +
+                        "and time, latitude, longitude");
+            }
+
+            Coordinate point = new Coordinate(Double.parseDouble(data[2]), Double.parseDouble(data[3]));
+            timestamp.put(Float.parseFloat(data[1]), point);
+        }
+        csvReader.close();
 
     }
 
-    private parsePointData(File file){
-
+    public void updatePositionToServer(String currentTime){
+        //we need to be careful -- given current time may JUST be off 0.1 s off our known times
+        if(timestamp.containsKey(Float.parseFloat(currentTime))){
+            //send position to server
+        }else{
+            //need a way to handle this
+        }
     }
 
 }
