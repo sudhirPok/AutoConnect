416b0ed79056abd317c157b73f878cff370c2e7f
Sudhir Pokhrel <sud7scholar@gmail.com>
2019-11-07 23:29:12 -0500

    added sample traffic data for 10 vehicles

--- AutoConnect/src/TrafficSimulation.java
+++ AutoConnect/src/TrafficSimulation.java
@@ -14,13 +14,20 @@ public class TrafficSimulation {
 
     private static ArrayList<Vehicle> vehicleArrayList = new ArrayList();
 
-    private static ArrayList<String> testingVehiclePath = new ArrayList<String>(
+    private static ArrayList<String> testingVehiclePath = new ArrayList<>(
             Arrays.asList("trafficData/vehicleData1.txt","trafficData/vehicleData2.txt","trafficData/vehicleData3.txt","trafficData/vehicleData4.txt",
             "trafficData/vehicleData5.txt", "trafficData/vehicleData6.txt", "trafficData/vehicleData7.txt", "trafficData/vehicleData8.txt",
             "trafficData/vehicleData9.txt","trafficData/vehicleData10.txt"));
 
     public static void main(String[] args){
 
+        initializeTraffic();
+
+        System.out.println("Yeah, works so far!");
+
+    }
+
+    private static void initializeTraffic(){
         try{
             //ArrayList<String> createdVehicleData = readTrafficData(rawTrafficData);
             ArrayList<String> createdVehicleData = testingVehiclePath;
@@ -30,9 +37,6 @@ public class TrafficSimulation {
         }catch(ParseVehicleDataException e){
             System.out.println(e.getMessage());
         }
-
-        System.out.println("Yeah, works so far!");
-
     }
 
     //This method takes in a CSV file containing the raw traffic simulation data for n vehicles,
