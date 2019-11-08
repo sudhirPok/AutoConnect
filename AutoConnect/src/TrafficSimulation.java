b52491120a1a8b745da1349254c3fc3d8251cd51
Sudhir Pokhrel <sud7scholar@gmail.com>
2019-11-07 22:06:50 -0500

    finished parsing raw traffic data into n individual vehicle csv's

--- AutoConnect/src/TrafficSimulation.java
+++ AutoConnect/src/TrafficSimulation.java
@@ -9,16 +9,25 @@ import java.util.ArrayList;
 
 public class TrafficSimulation {
     private static final String rawTrafficData = "trafficData/kathmanduTraffic.txt";
+    private static final int numVehicles = 10;
 
     private static ArrayList<Vehicle> vehicleArrayList = new ArrayList();
+    /*
+    private static ArrayList<String> testingVehiclePath = new ArrayList(){
+        "trafficData/vehicleData1.txt","trafficData/vehicleData2.txt","trafficData/vehicleData3.txt","trafficData/vehicleData4.txt"
+                "trafficData/vehicleData5.txt", "trafficData/vehicleData6.txt", "trafficData/vehicleData7.txt", "trafficData/vehicleData8.txt"
+                "trafficData/vehicleData9.txt","trafficData/vehicleData10.txt"
+    };*/
 
 
     public static void main(String[] args){
 
         try{
-            ArrayList<String> createdVehicledData = readTrafficData(rawTrafficData);
-            createVehicles(createdVehicledData);
+            ArrayList<String> createdVehicleData = readTrafficData(rawTrafficData);
+            createVehicles(createdVehicleData);
         }catch(IOException e){
+            System.out.println(e.getMessage());
+        }catch(ParseVehicleDataException e){
             System.out.println(e.getMessage());
         }
 
@@ -36,25 +45,25 @@ public class TrafficSimulation {
 
         FileWriter writer = null;
 
-        int counter = 0;
-        while((row = csvReader.readLine()) != null){
+        int countVehicles = 0;
+        while((row = csvReader.readLine()) != null && countVehicles<=numVehicles){
             String[] data = row.split(",");
 
             //if we encounter a car id, stop writing to current file & write to new file.
             //note, this skips the current line
-            if (!data[0].isEmpty()){
+            if (!data[0].isEmpty() && countVehicles!=numVehicles){
                 if(writer!=null){
                     writer.flush();
                     writer.close();
                 }
 
-                String filePath = "trafficData/vehicleData" + Integer.toString(++counter) + ".txt";
+                String filePath = "trafficData/vehicleData" + Integer.toString(++countVehicles) + ".txt";
                 Path vehicleFilePath = Paths.get(filePath);
                 Files.createFile(vehicleFilePath);
                 vehicleData.add(filePath);
                 writer = new FileWriter(filePath);
             }else{
-                writer.write(row);
+                writer.write(row + "\n");
             }
         }
 
@@ -64,10 +73,14 @@ public class TrafficSimulation {
         return vehicleData;
     }
 
-    private static void createVehicles(ArrayList<String> vehicleCSVs){
+    private static void createVehicles(ArrayList<String> vehicleCSVs) throws ParseVehicleDataException{
         for(String csvFile: vehicleCSVs){
             Vehicle vehicle = new Vehicle(csvFile);
             vehicleArrayList.add(vehicle);
+        }
+
+        if(vehicleArrayList.size()!=numVehicles){
+            throw new ParseVehicleDataException(String.format("Raw traffic data was parsed, but not into %s individual vehicles!", numVehicles));
         }
     }
 
